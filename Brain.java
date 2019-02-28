fab443ae196fa841ec4964dfcbf9ec0b88b4214d
Oscar Almqvist <oscaralm@kth.se>
2019-02-28 19:10:48 +0100

    now compiles

--- Brain.java
+++ Brain.java
@@ -4,15 +4,15 @@ public class Brain {
 
   public static int getScore(Photo photo1, Photo photo2) {
     HashSet<String> inPhoto1 = new HashSet<>();
-    for (int i = 0; i < photo1.size(); i++) {
-      inPhoto1.add(photo1.get(i));
+    for (int i = 0; i < photo1.tags.size(); i++) {
+      inPhoto1.add(photo1.tags.get(i));
     }
 
     int same = 0;
-    int different = photo1.size() + photo2.size();
+    int different = photo1.tags.size() + photo2.tags.size();
 
-    for (int i = 0; i < photo2.size(); i++) {
-      if (inPhoto1.contains(photo2.get(i))) {
+    for (int i = 0; i < photo2.tags.size(); i++) {
+      if (inPhoto1.contains(photo2.tags.get(i))) {
         same++;
         different -= 2;
       }
