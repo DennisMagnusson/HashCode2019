6d9a12b59486e0f6100494052a765e865dd3df93
d <dennis.magnusson98@gmail.com>
2019-02-28 19:07:34 +0100

    This compiles

--- Test.java
+++ Test.java
@@ -23,21 +23,25 @@ public class Test {
     } catch(Exception e) {}
     
     int nPhotos = Integer.parseInt(lines.remove(0));
-    Photo[] photos = new Photos[nPhotos];
+    Photo[] photos = new Photo[nPhotos];
     int z = 0;
     for(String line : lines) {
       String[] things = line.split(" ");
       boolean horizontal = (things[0] == "H");
       int i = Integer.parseInt(things[1]);
-      ArrayList<Strings> tags = new ArrayList<>();
+      ArrayList<String> tags = new ArrayList<>();
       for(int u = 0; u < i; i++) {
         tags.add(things[i+2]);
       }
-      photos[z] = new Photo(horizontal, tags, z);
+      photos[z] = new Photo();
+      photos[z].id1 = z;
+      photos[z].isVertical = !horizontal;
+      photos[z].tags = tags;
+
       z++;
     }
 
-    ArrayList<Photo> arr = new ArrayList<>();
+    ArrayList<Photo> arr = new ArrayList<>();//READ THIS FROM SOMEWHERE
     System.out.println(arr.size());
     for(Photo p : arr) {
       if(p.id2 == -1) System.out.println(p.id1);
